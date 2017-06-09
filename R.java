e00b775852d012571a23a0ca851d60fa4ad46e36
ritesh-bst <ritesh@bluestacks.com>
2017-06-09 13:43:45 +0530

    added

--- R.java
+++ R.java
@@ -62,4 +62,6 @@ public final class R {
         public static final int hello=0x7f040000;
         public static final int strokewidth=0x7f040002;
     }
+ public static final class new {
+ public static final int rr=1;
 }
